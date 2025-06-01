99874a5ec6866c9b964029b7ec5a6906221afc64
pv3216 <pv3216@gmail.com>
2025-06-01 13:40:38 +0530

    UPDATE: randered table

--- src/page/repo/repo.jsx
+++ src/page/repo/repo.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Grid, Column } from "@carbon/react";
+import RepoTable from "./repoTable";
+import { headers, rows } from "./repoStaticTable";
 
 function Repo() {
-  return <div>Repo Page</div>;
+  return (
+    <Grid className="repo-page">
+      <Column lg={16} md={8} sm={4} className="repo-page__r1">
+        <RepoTable rows={rows} headers={headers} />
+      </Column>
+    </Grid>
+  );
 }
 
 export default Repo;
